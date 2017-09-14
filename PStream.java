c5a50800dcea3c1df290756b1194e29187ecaac0
xusanduo08 <2279190887@qq.com>
2017-09-14 22:35:01 +0800

    重定向标准输入和输出

--- PStream.java
+++ PStream.java
@@ -1,11 +1,15 @@
 package com.mengfansheng.IO;
 
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 
 public class PStream {
@@ -18,6 +22,14 @@ public class PStream {
 		
 		//设置输出到控制台
 		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(FileDescriptor.out)),true));
+		
+		//设置标准输入流从文件输入
+		System.setIn(new BufferedInputStream(new FileInputStream(new File("E:/Git/tt.txt"))));
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine());
+		
+		//设置标准输入流冲控制台输入
+		System.setIn(new BufferedInputStream(new FileInputStream(FileDescriptor.in)));
 		
 	}
 
