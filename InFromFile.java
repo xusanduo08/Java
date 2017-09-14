066a70ec90a81f5fd2f830436be39ac49b54c1f4
xusanduo08 <2279190887@qq.com>
2017-09-14 22:22:17 +0800

    关闭流

--- InFromFile.java
+++ InFromFile.java
@@ -4,17 +4,19 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class InFromFile {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		//标准键盘输入 ==》 从文件输入
 		InputStream is = System.in;
 		is = new BufferedInputStream(new FileInputStream(new File("E:/Git/rr.txt")));
 		Scanner sc = new Scanner(is);
 		System.out.println(sc.nextLine());
+		is.close();
 	}
 
 }
