c122b8e98da2fa9e1fb7bbe129ae7cf1f0d6177f
xusanduo08 <2279190887@qq.com>
2017-09-19 23:50:16 +0800

    加入文件合并两种操作，一种使用正常输出流，一种使用SequenceInputStream

--- SplitFile.java
+++ SplitFile.java
@@ -1,20 +1,28 @@
 package com.mengfansheng.IO;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.SequenceInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+
+import org.omg.CORBA_2_3.portable.InputStream;
 
 public class SplitFile {
 
 	public static void main(String[] args) throws IOException {
 
-		SplitFile split = new SplitFile("E:/Git/ww.rmvb", 500000);
-		split.split("E:/Git");
+		SplitFile split = new SplitFile("E:/gg/ff.rmvb", 5000000,"E:/gg");
+		//split.split();
+		//split.mergeFile("E:/gg/merge/gg.rmvb");
+		split.merge("E:/gg/merge/ggg.rmvb");
 	}
 
 	//文件路径
@@ -30,19 +38,22 @@ public class SplitFile {
 	private long blockSize;
 	//每一块名称
 	private List<String> blockPath;
+	//分割后的路径
+	private String destPath;
 	
 	public SplitFile(){
 		blockPath = new ArrayList<String>();
 	}
 	
-	public SplitFile(String filePath){
-		this(filePath, 1024);
+	public SplitFile(String filePath,String destPath){
+		this(filePath, 1024, destPath);
 	}
 	
-	public SplitFile(String filePath, long blockSize){
+	public SplitFile(String filePath, long blockSize, String destPath){
 		this();
 		this.filePath = filePath;
 		this.blockSize = blockSize;
+		this.destPath = destPath;
 		init();
 	}
 	
@@ -67,12 +78,13 @@ public class SplitFile {
 		}
 		//确定块数
 		this.size = (int)Math.ceil(length*1.0 / this.blockSize);
+		initPathName();
 	}
 	
-	//每一块名称
-	private void initPathName(String destPath){
+	//计算出每一块名称
+	private void initPathName(){
 		for(int i = 0; i < this.size; i++){
-			this.blockPath.add(destPath + "/" + this.fileName + ".part" + i);
+			this.blockPath.add(this.destPath + "/" + this.fileName + ".part" + i);
 		}
 	}
 	
@@ -81,10 +93,10 @@ public class SplitFile {
 	 * 起始位置
 	 * 实际大小
 	 * */
-	public void split(String dest) throws IOException{
+	public void split() throws IOException{
 		long beginPos = 0;
 		long actualSize = blockSize;//每块实际大小
-		initPathName(dest);
+		
 		for(int i  = 0; i< this.size; i++){
 			if(i == this.size - 1){
 				actualSize = this.length - beginPos;
@@ -119,4 +131,60 @@ public class SplitFile {
 		raf.close();
 	}
 	
+	public void mergeFile(String destPath) throws IOException{
+		//创建源
+		File dest = new File(destPath);
+		//选择流
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		bos = new BufferedOutputStream(new FileOutputStream(dest,true));//追加方式写入
+		for(int i = 0; i < this.blockPath.size(); i++){
+			File blockFile = new File(this.blockPath.get(i));
+			if(!blockFile.exists() || blockFile.isDirectory()){
+				System.out.println("请先分割文件后合并");
+				return;
+			}
+			bis = new BufferedInputStream(new FileInputStream(new File(this.blockPath.get(i))));
+			byte[] flush = new byte[1024];
+			int len = 0;
+			while(-1 != (len = bis.read(flush))){
+				bos.write(flush, 0, len);
+			}
+			
+			FileClose.close( bis);
+		}
+		
+		FileClose.close(bos);
+	}
+	
+	
+	public void merge(String destPath) throws IOException{
+		Vector<BufferedInputStream> vi = new Vector<BufferedInputStream>();
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(destPath),true));
+		System.out.println(this.blockPath.size());
+		for(int i = 0;i < this.blockPath.size(); i++){
+			BufferedInputStream bis = null;
+			try {
+				File blockFile = new File(this.blockPath.get(i));
+				if(blockFile.exists() && !blockFile.isDirectory()){
+					bis = new BufferedInputStream(new FileInputStream(blockFile));
+					vi.add(bis);
+				} else {
+					System.out.println("请先进行分割文件操作,然后再进行文件合并");
+					return;
+				}
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		SequenceInputStream sis = new SequenceInputStream(vi.elements());
+		byte[] flush = new byte[1024];
+		int len = 0;
+		while(-1 != (len = sis.read(flush))){
+			bos.write(flush, 0, len);
+		}
+		bos.flush();
+		FileClose.close(sis,bos);
+	}
 }
