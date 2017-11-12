671dbc0d8d52deaa12935372680f4950bdaeece4
xusanduo08 <2279190887@qq.com>
2017-11-12 22:56:52 +0800

    注册消息的工厂类

---
+++ 手写可以发送响应消息的服务器/RegisterServlet.java
@@ -0,0 +1,19 @@
+package com.mengfansheng.net;
+/**
+ * 注册的工厂类
+ * **/
+public class RegisterServlet extends Servlet {
+
+	@Override
+	public void doGet(Request req, Response rep) throws Exception {
+		
+	}
+
+	@Override
+	public void doPost(Request req, Response rep) throws Exception {
+		rep.print("<!DOCTYPE html><html><head><title>注册结果</title></head>");
+		rep.print("<body>Hello,欢迎").print(req.getParamter("uname")).print(" 注册成功</body></html>");
+		
+	}
+
+}
