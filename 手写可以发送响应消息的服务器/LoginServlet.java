c74fe8d158dec8defe2f7a41359e788a127b24a7
xusanduo08 <2279190887@qq.com>
2017-11-12 22:56:33 +0800

    登录消息的工厂类

---
+++ 手写可以发送响应消息的服务器/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.mengfansheng.net;
+
+/**
+ * 登录响应信息的的工厂类
+ * **/
+public class LoginServlet extends Servlet {
+
+	@Override
+	public void doGet(Request req, Response rep) throws Exception {
+		rep.print("<!DOCTYPE html><html><head><title>表单提交</title></head>");
+		rep.print("<body>Hello,欢迎").print(req.getParamter("uname")).print("</body></html>");
+		
+		String name = req.getParamter("uname");
+		String pwd = req.getParamter("pwd");
+		
+		if(login(name, pwd)){
+			rep.print("<!DOCTYPE html><html><head><title>表单提交</title></head>");
+			rep.print("<body>Hello,欢迎").print(req.getParamter("uname")).print("</body></html>");
+		} else {
+			rep.print("<!DOCTYPE html><html><head><title>表单提交</title></head>");
+			rep.print("<body>登录失败</body></html>");
+		}
+		
+	}
+
+	/**
+	 * @desc 判断用户名和密码是否正确
+	 * @param String name, String pwd
+	 * @return boolean true 登录成功  false登录失败
+	 * **/
+	public boolean login(String name, String pwd){
+		if(null != name && null != pwd){
+			return name.equals("admin") && pwd.equals("123456");
+		}
+		return false;
+	}
+	
+	@Override
+	public void doPost(Request req, Response rep) throws Exception {
+		
+	}
+	
+}
