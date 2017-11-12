079bffdbc97860bf36b588aa697163f741db82af
xusanduo08 <2279190887@qq.com>
2017-11-12 22:55:26 +0800

    改为抽象类，便于子类继承以应该多种请求情况

--- 手写可以发送响应消息的服务器/Servlet.java
+++ 手写可以发送响应消息的服务器/Servlet.java
@@ -1,16 +1,19 @@
 package com.mengfansheng.net;
 /**
- * @desc 进一步封装，将具体的响应内容生成动作封装到类中
+ * @desc 一个抽象类，便于子类继承去生成对应不同上下文的返回信息
  * **/
-public class Servlet {
+public abstract class Servlet {
 	
 	/**
 	 * @desc 拼接响应消息体
 	 * @param Request req, Response rep
-	 * 
+	 * @throws Exception 
 	 * **/
-	public void service(Request req, Response rep){
-		rep.print("<!DOCTYPE html><html><head><title>表单提交</title></head>");
-		rep.print("<body>Hello,欢迎").print(req.getParamter("uname")).print("</body></html>");
+	public void service(Request req, Response rep) throws Exception{
+		this.doGet(req, rep);
+		this.doPost(req, rep);
 	}
+	
+	public abstract void doGet(Request req, Response rep) throws Exception;
+	public abstract void doPost(Request req, Response rep) throws Exception;
 }
