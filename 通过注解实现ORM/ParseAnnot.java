a90f06b438039d59601859d29e9bc609cffebd33
xusanduo08 <2279190887@qq.com>
2017-11-26 11:44:18 +0800

    通过反射获得类的属性的注解

--- 通过注解实现ORM/ParseAnnot.java
+++ 通过注解实现ORM/ParseAnnot.java
@@ -23,8 +23,8 @@ public class ParseAnnot {
 			System.out.println(st.value());
 			
 			//获得类的属性的注解
-			Field f = clz.getDeclaredField("name");
-			TableField tb = f.getAnnotation(TableField.class);
+			Field f = clz.getDeclaredField("name");//先通过反射获得某个属性
+			TableField tb = f.getAnnotation(TableField.class);//然后获得这个属性的指定类的注解
 			//这样就能获得指定属性在数据库中对应的字段名称、类型以及字段长度
 			System.out.println(tb.columnName() +"--"+tb.type() +"--"+tb.length());
 			
