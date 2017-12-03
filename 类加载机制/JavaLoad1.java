509a00e23409e738f19d6d601dfe55432f9776cc
xusanduo08 <2279190887@qq.com>
2017-12-03 10:25:40 +0800

    自定义一个java.lang.Object，测试双亲委托机制

--- 类加载机制/JavaLoad1.java
+++ 类加载机制/JavaLoad1.java
@@ -12,6 +12,14 @@ public class JavaLoad1 {
 		//C:\Users\Administrator\workspace\JavaLoad\bin，也就是说，当前系统加载类的时候是按照这个路径加载的
 		//这个路径各个项目是独立的，所以运行时项目之间不会相互影响
 		System.out.println(System.getProperty("java.class.path"));
+		
+		System.out.println("*****************测试双亲加载机制******************");
+		String a = "gogo";
+		//获得a这个类的加载器
+		//获取的类加载器是null，因为采用的是双亲委托加载机制，并且String这个类式在java的核心包rt.jar中，所以最终使用的是引导类加载器来加载这个类
+		//我们自定义的String类并没有被加载
+		System.out.println(a.getClass().getClassLoader());
+		System.out.println(a);
 	}
 
 }
