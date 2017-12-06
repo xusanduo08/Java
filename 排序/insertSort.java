9f8931d9f05504911eb87443c8083ceec21430c6
xusanduo08 <2279190887@qq.com>
2017-12-06 22:16:17 +0800

    插入排序

---
+++ 排序/insertSort.java
@@ -0,0 +1,35 @@
+package com.mengfansheng.javaload;
+
+import java.util.Arrays;
+
+/**
+ * 数组分为有序部分和无序部分，有序部分在左，无序部分在右
+ * 一开始，有序部分只有a[0]，剩下的都是无序部分
+ * 遍历无序部分，插入到有序部分合适的位置
+ * */
+public class insertSort {
+
+	public static void main(String[] args){
+		int a[] = {3,2,4,7,5,1};
+		int b[] = insert(a.length, a);
+		System.out.println(Arrays.toString(b));
+	}
+	
+	public static int[] insert(int size, int[] a){
+		for(int i = 1; i < size; i++){
+			for(int j = 0; j < i; j++){
+				if(a[i] < a[j]){
+					int temp = a[i];
+					for(int k = i; k>j; k--){//将j（包含j）位置到i（包含i）位置的数据往后移一位
+						a[k] = a[k - 1];
+					}
+					a[j] = temp;
+					break;
+				}
+			}
+		}
+		return a;
+		
+	}
+	
+}
